51e618cb88c0c96b59aeea2709a987a60642eae6
XdrfvgzUser <59778595+XdrfvgzUser@users.noreply.github.com>
2023-06-09 22:35:34 +0200

    Create wget.sh

---
+++ wget.sh
@@ -0,0 +1,4 @@
+wget -q https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz -O - | tar xzf - mkbootimg.py gki
+chmod +x mkbootimg.py
+sudo mv mkbootimg.py /usr/local/bin/mkbootimg
+sudo mv gki $(python -c 'import site; print(site.getsitepackages()[0])')
