68846e8a429c88ca8a640a2d159c9f3efc9f8718
XdrfvgzUser <59778595+XdrfvgzUser@users.noreply.github.com>
2023-06-09 22:56:19 +0200

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget -q https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz -O - | tar xzf - mkbootimg.py gki
 chmod +x mkbootimg.py
 sudo mv mkbootimg.py /usr/local/bin/mkbootimg
