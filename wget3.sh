7ca34b367ddce9d9be6ec2fb89a1f5aed7f91422
XdrfvgzUser <59778595+XdrfvgzUser@users.noreply.github.com>
2023-06-10 21:01:35 +0200

    Delete wget3.sh

--- wget3.sh
+++
@@ -1,3 +0,0 @@
-wget -q https://android.googlesource.com/platform/external/avb/+archive/refs/heads/master.tar.gz -O - | tar xzf - avbtool.py
-chmod +x avbtool.py
-sudo mv avbtool.py /usr/local/bin/avbtool
