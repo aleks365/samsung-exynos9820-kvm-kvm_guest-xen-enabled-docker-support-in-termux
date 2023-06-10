c532973cb39572492d909baff3cbf5c9eaff7cae
XdrfvgzUser <59778595+XdrfvgzUser@users.noreply.github.com>
2023-06-10 21:01:16 +0200

    Delete wget2.sh

--- wget2.sh
+++
@@ -1,4 +0,0 @@
-wget -q https://android.googlesource.com/platform/system/libufdt/+archive/refs/heads/master.tar.gz -O - | tar --strip-components 2 -xzf - utils/src/mkdtboimg.py
-chmod +x mkdtboimg.py
-sudo mv mkdtboimg.py /usr/local/bin/mkdtboimg
-      
